d71771379cfebe9d6357e9e5acbc0496219420ed
Dev-Yesung <yeasung67@gmail.com>
2024-10-31 21:40:25 +0900

    [monolithic] setup

--- backend/monolithic/core/build.gradle.kts
+++ backend/monolithic/core/build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
 }
 
 group = "com.wave"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
--- backend/monolithic/settings.gradle.kts
+++ backend/monolithic/settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.name = "monolithic"
+include("core")
+include("common")
+include("application")
+include("port")
+include("adapter")
